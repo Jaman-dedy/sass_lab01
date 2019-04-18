98a556e7374e3e7b8d6b80514d8e82fe0457f0c4
Jaman dedy <jeandedieuam@gmail.com>
2019-04-18 21:48:59 +0200

    first step in sass

---
+++ views/js/main.js
@@ -0,0 +1,18 @@
+const menuBtn = document.querySelector('.btn-menu');
+const menu = document.querySelector('.menu');
+const menuNav = document.querySelector('.menu-nav');
+const menuBranding = document.querySelector('.menu-branding');
+
+const navItems = document.querySelectorAll('.nav-items');
+
+let showMenu = false;
+
+menuBtn.addEventListener('click', toggleMenu);
+
+function toggleMenu() {
+    if(!showMenu) {
+    menuBtn.classList.add('')
+    } else {
+
+    }
+}
